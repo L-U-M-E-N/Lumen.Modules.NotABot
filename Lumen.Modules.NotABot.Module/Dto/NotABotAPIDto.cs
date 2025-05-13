0774d1baf7a546de5f4ba8ede8de0cb2e3b2ba59
Elanis <elanis@hotmail.com>
2025-05-13 22:57:25 +0200

    Fix Json deserialize when the "reactions", "channels" or "users" field is empty

--- Lumen.Modules.NotABot.Module/Dto/NotABotAPIDto.cs
+++ Lumen.Modules.NotABot.Module/Dto/NotABotAPIDto.cs
@@ -1,4 +1,8 @@
-﻿namespace Lumen.Modules.NotABot.Module.Dto {
+﻿using Dysnomia.Common.WebAPIWrapper.Helpers;
+
+using System.Text.Json.Serialization;
+
+namespace Lumen.Modules.NotABot.Module.Dto {
     public class NotABotAPIDto {
         public long Date { get; set; }
         public int MemberLeft { get; set; }
@@ -8,8 +12,11 @@
         public int ReactionRemoved { get; set; }
         public int MessageCount { get; set; }
 
+        [JsonConverter(typeof(EmptyArrayToObjectConverter<Dictionary<string, NotABotAPIChannelDto>>))] // NaB can return [] here ...
         public IDictionary<string, NotABotAPIChannelDto> Channels { get; set; } = null!;
+        [JsonConverter(typeof(EmptyArrayToObjectConverter<Dictionary<string, NotABotAPIReactionDto>>))] // NaB can return [] here ...
         public IDictionary<string, NotABotAPIReactionDto> Reactions { get; set; } = null!;
+        [JsonConverter(typeof(EmptyArrayToObjectConverter<Dictionary<string, NotABotAPIUserDto>>))] // NaB can return [] here ...
         public IDictionary<string, NotABotAPIUserDto> Users { get; set; } = null!;
     }
 }
